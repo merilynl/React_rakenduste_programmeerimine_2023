465ae78935ebc719596cd047602f3b507ffd1f40
Merilyn Litvinov <merilynl@users.noreply.github.com>
2023-10-10 19:16:59 +0300

    first part of 27.09 homework

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import { useState } from "react"
-import Toggle from "./components/Toggle"
-import Show from "./components/Show"
-import Maths from "./components/Maths"
-import MyForm from "./components/MyForm"
-import PropDrilling from "./components/PropDrilling"
-import Context from "./components/Context"
-import Fetching from "./components/Fetching"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import Layout from "./layout/Layout"
+import { Outlet } from "react-router-dom"
+import Header from "./layout/Header"
+import { useLocation } from "react-router-dom"
+import { Typography } from "@mui/material"
 import "./App.css"
 
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "rgb(254, 217, 255)"
+    }
+  }
+})
+
 function App() {
-  const [show, setShow] = useState(true)
-  const toggleShow = () => setShow(previousShow => !previousShow)
+  let location = useLocation()
   return (
-    <div className="Container">
-      <h1>Merilyn</h1>
-      <Fetching />
-      <PropDrilling />
-      <Context />
-      <MyForm />
-      <Maths />
-      <Show show={show} />
-      <Toggle toggleShow={toggleShow} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header title="Kekw" />
+      <Layout>
+        {location?.pathname === "/" && (
+          <Typography
+            variant="h4"
+            sx={{ marginLeft: 10, marginTop: 5 }}
+          >
+            Merilyn Hunt
+          </Typography>
+        )}
+        <Outlet />
+      </Layout>
+    </ThemeProvider>
   )
 }
 
